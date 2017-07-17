f92ed5f67feeeca0550e061486adaffd60e31f9a
Dolan <dolan_miu@hotmail.com>
2017-07-17 21:30:45 +0100

    Exported other components

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-import { NgColorModule } from './color-picker.module';
-
-export { NgColorModule }
+export { NgColorModule } from './color-picker.module';
+export { ColorPickerComponent } from './color-picker/standard-picker.component';
+export { ColorPreviewPickerComponent } from './color-picker/standard-preview-picker.component';
