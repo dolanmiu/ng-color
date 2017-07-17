b0e70649fb71970ffced307b1fb608d2171924f3
Dolan <dolan_miu@hotmail.com>
2017-07-17 22:10:23 +0100

    Exported all components from module

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
 export { NgColorModule } from './color-picker.module';
+
 export { ColorPickerComponent } from './color-picker/standard-picker.component';
 export { ColorPreviewPickerComponent } from './color-picker/standard-preview-picker.component';
+export { CursorPreviewComponent } from './cursor/cursor-preview.component';
+export { CursorComponent } from './cursor/cursor.component';
+export { HueRoundedComponent } from './hue/hue-rounded.component';
+export { HueComponent } from './hue/hue.component';
+export { MouseHandlerDirective } from './mouse-handler.directive';
+export { SaturationLightnessComponent } from './saturation-lightness-box/saturation-lightness-box.component';
+export { SaturationLightnessPreviewComponent } from './saturation-lightness-box/saturation-lightness-preview.component';
+export { ColorService } from './services/color.service';
