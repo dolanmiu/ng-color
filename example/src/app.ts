cba526807698ff12d20b3e198f06275858c25911
Dolan <dolan_miu@hotmail.com>
2017-11-21 23:07:16 +0000

    Update examples

--- example/src/app.ts
+++ example/src/app.ts
@@ -9,35 +9,33 @@ import { NgColorModule } from 'ng-color';
     template: `
         <div>
             <h1>Hello {{name}}</h1>
-            <h2>Style 1</h2>
-            <h3>ng-color-basic</h3>
+            <h2>Box</h2>
+            <h3>ng-color-box</h3>
             <h4>Using startHex</h4>
-            <ng-color-basic (colorChange)="colorChange($event)" [(ngModel)]="dench" startHex="DEADBF"></ng-color-basic>
-            <div [style.background-color]="hexColor">Output Color: {{hexColor}}</div>
+            <ng-color-box (colorChange)="colorChange($event)" [(ngModel)]="dench" startHex="DEADBF"></ng-color-box>
+            <div [style.background-color]="color ? color.hexString : ''">Output Color: {{color.hexString}}</div>
             {{color | json}}
             {{dench | json}}
 
             <h4>Not using startHex</h4>
-            <ng-color-basic></ng-color-basic>
+            <ng-color-box></ng-color-box>
 
-            <h2>Style 2</h2>
-            <h3>ng-color-basic-preview</h3>
+            <h4>Setting size</h4>
+            <ng-color-box [(ngModel)]="test6" style="width: 500px;"></ng-color-box>
+            {{test6 | json}}
+
+            <h2>Circle</h2>            
             <h4>Using startHex</h4>
-            <ng-color-basic-preview (colorChange)="colorChange2($event)" [(ngModel)]="dench2" startHex="23bf26"></ng-color-basic-preview>
-            {{color2 | json}}
-            {{dench2 | json}}
+            <ng-color-circle [(ngModel)]="test5" startHex="23bf26"></ng-color-circle>
+            {{test5 | json}}
 
+            <h4>Not using startHex</h4>            
             <ng-color-circle [(ngModel)]="test3"></ng-color-circle>
             {{test3 | json}}
 
+            <h4>Setting size</h4>
             <ng-color-circle [(ngModel)]="test4" startHex="23bf26" style="width: 300px; height: 300px;"></ng-color-circle>
             {{test4 | json}}
-            
-            <ng-color-box [(ngModel)]="test5" startHex="23bf26"></ng-color-box>
-            {{test5 | json}}
-
-            <ng-color-box [(ngModel)]="test6"></ng-color-box>
-            {{test6 | json}}
         </div>
     `,
 })
@@ -59,13 +57,6 @@ export class AppComponent {
 
     public colorChange2(color: ColorOutput): void {
         this.color2 = color;
-    }
-
-    public get hexColor(): string {
-        if (this.color === undefined) {
-            return '';
-        }
-        return this.color.hexString;
     }
 }
 
