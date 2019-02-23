b9a1df30c096c4a6143b6d90c4e3dee49d4360d0
Dolan Miu <dolan_miu@hotmail.com>
2019-02-23 01:42:42 +0000

    Fix example and update package lock

--- example/config.js
+++ example/config.js
@@ -1,51 +1,57 @@
 System.config({
-  //use typescript for compilation
-  transpiler: 'typescript',
-  //typescript compiler options
-  typescriptOptions: {
-    emitDecoratorMetadata: true
-  },
-  paths: {
-    'npm:': 'https://unpkg.com/'
-  },
-  //map tells the System loader where to look for things
-  map: {
-    
-    'app': './src',
-    
-    '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
-    '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
-    '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
-    '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
-    '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
-    '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
-    '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
-    '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
-    
-    '@angular/core/testing': 'npm:@angular/core/bundles/core-testing.umd.js',
-    '@angular/common/testing': 'npm:@angular/common/bundles/common-testing.umd.js',
-    '@angular/compiler/testing': 'npm:@angular/compiler/bundles/compiler-testing.umd.js',
-    '@angular/platform-browser/testing': 'npm:@angular/platform-browser/bundles/platform-browser-testing.umd.js',
-    '@angular/platform-browser-dynamic/testing': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic-testing.umd.js',
-    '@angular/http/testing': 'npm:@angular/http/bundles/http-testing.umd.js',
-    '@angular/router/testing': 'npm:@angular/router/bundles/router-testing.umd.js',
-
-    'ng-color': './ng-color/ng-color.js',
-    
-    'color-name': 'npm:color-name',
-    'color-convert': 'npm:color-convert/conversions.js',
-
-    'rxjs': 'npm:rxjs',
-    'typescript': 'npm:typescript@2.0.2/lib/typescript.js'
-  },
-  //packages defines our app package
-  packages: {
-    app: {
-      main: './main.ts',
-      defaultExtension: 'ts'
+    //use typescript for compilation
+    transpiler: 'typescript',
+    //typescript compiler options
+    typescriptOptions: {
+        emitDecoratorMetadata: true,
     },
-    rxjs: {
-      defaultExtension: 'js'
-    }
-  }
+    paths: {
+        'npm:': 'https://unpkg.com/',
+    },
+    //map tells the System loader where to look for things
+    map: {
+        app: './src',
+
+        '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
+        '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
+        '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
+        '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
+        '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
+        '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
+        '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
+        '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
+
+        '@angular/core/testing': 'npm:@angular/core/bundles/core-testing.umd.js',
+        '@angular/common/testing': 'npm:@angular/common/bundles/common-testing.umd.js',
+        '@angular/compiler/testing': 'npm:@angular/compiler/bundles/compiler-testing.umd.js',
+        '@angular/platform-browser/testing': 'npm:@angular/platform-browser/bundles/platform-browser-testing.umd.js',
+        '@angular/platform-browser-dynamic/testing':
+            'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic-testing.umd.js',
+        '@angular/http/testing': 'npm:@angular/http/bundles/http-testing.umd.js',
+        '@angular/router/testing': 'npm:@angular/router/bundles/router-testing.umd.js',
+
+        'ng-color': './ng-color/ng-color.js',
+
+        'color-name': 'npm:color-name',
+        'color-convert': 'npm:color-convert/conversions.js',
+
+        rxjs: 'npm:rxjs',
+        typescript: 'npm:typescript@2.0.2/lib/typescript.js',
+    },
+    //packages defines our app package
+    packages: {
+        app: {
+            main: './main.ts',
+            defaultExtension: 'ts',
+        },
+        // https://github.com/ReactiveX/rxjs/issues/2971#issuecomment-392979323
+        rxjs: {
+            main: 'index.js',
+            defaultExtension: 'js',
+        },
+        'rxjs/operators': {
+            main: 'index.js',
+            defaultExtension: 'js',
+        },
+    },
 });
